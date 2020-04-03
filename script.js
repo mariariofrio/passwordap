df758bb63a5a9494b8b2fa2e0a1da5411aa1f725
majo <maria.riofrio.mr@gmail.com>
2020-04-02 21:58:13 -0400

    current work

--- script.js
+++ script.js
@@ -4,90 +4,99 @@ var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var simbols = ['!', '@', '#', '$', '%', '^', '&', '*'];
 
 // function to prompt user for password
-
+var passwordLenght;
+var character;
 function passwordPrompt() {
-  var passwordlenght = parseInt(prompt("how many characters would you like your password to be"));
-  if (isNaN(passwordlenght) === true) {
+  passwordLenght = parseInt(prompt("how many characters would you like your password to be"));
+  if (isNaN(passwordLenght) === true) {
     alert("passwordlenght must be a number");
     return;
   }
-  if (passwordlenght < 8 || passwordlenght > 128) {
+  if (passwordLenght < 8 || passwordLenght > 128) {
     alert("passwordlenght must be at least 8 characters");
     return;
   }
+  var specialcharacter = confirm("click to confirm your password has a special character");
+  var lowercasecheck = confirm("click to confirm lowercase");
+  var uppercasecheck = confirm("click to confirm uppercase");
+  var numbercheck = confirm("click to confirm number");
+  if (
+    specialcharacter === false &&
+    lowercasecheck === false &&
+    uppercasecheck === false &&
+    numbercheck === false
+  ){
+    alert("password must have special,lowercase, uppercase or a number character");
+     return;
+  }
+  // this is an object that is storing the user input
+  
+  var confirmPassword = {
+    specialcharacter: specialcharacter,
+    lowercasecheck: lowercasecheck,
+    uppercasecheck: uppercasecheck,
+    numbercheck: numbercheck,
+    passwordLenght: passwordLenght,
+  }
+  return confirmPassword
 }
 
-var specialcharacter = confirm("click to confirm your password has a special character");
-var lowercasecheck = confirm("click to confirm lowercase");
-var uppercasecheck = confirm("click to confirm uppercase");
-var numbercheck = confirm("click to confirm number");
 
 
-if (
-  specialcharacter === false &&
-  lowercasecheck === false &&
-  uppercasecheck === false &&
-  numbercheck === false
-) 
-
-{
-  alert("password must have special,lowercase, uppercase or a number character");
-  // return;
-}
-// this is an object that is storing the user input
-
-var confirmPassword = {
-  specialcharacter: specialcharacter,
-  lowercasecheck: lowercasecheck,
-  uppercasecheck: uppercasecheck,
-  numbercheck: numbercheck,
-  passwordLenght: passwordLenght,
-}
 
 // spot for fuction to get a random element for an array
 function random_item(items) {
   var index = Math.floor(Math.random() * items.length);
-  var element = item[index];
+  var element = items[index];
   return element;
+  
 }
 // writing a function to generate the password based on users input
 
 function generatePassword() {
   var closing = passwordPrompt();
+  console.log("are we here",closing)
   //  variable to store the password
   var storing = [];
   // aaray to store characters
   var character = [];
   // make sure each type is used
-  var type = [];
-
+  //var type = [];
+  debugger
+  
   if (closing.specialcharacter) {
     character = character.concat(simbols);
-    type.push(random_item(simbols));
+    //type.push(random_item(simbols));
   }
+  debugger
   if (closing.lowercasecheck) {
     character = character.concat(lowercase);
-    type.push(random_item(lowercase));
+    //type.push(random_item(lowercase));
   }
+  debugger
   if (closing.uppercasecheck) {
     character = character.concat(uppercase);
-    type.push(random_item(uppercase));
+   // type.push(random_item(uppercase));
   }
+  debugger
   if (closing.numbercheck) {
     character = character.concat(numbers);
-    type.push(random_item(numbers));
+   // type.push(random_item(numbers));
   }
+  debugger
+ 
+  for (var i = 0; i < passwordLenght; i++) {
+    const randomNumber = Math.floor(Math.random() * character.length);
+    const randomLetter = character[randomNumber];
+    storing = storing.concat(randomLetter);
+    debugger
+  } 
+  password = storing.join('');
+  return password
 }
 
-for (var i = 0; i < passwordLenght; i++) {
+
   
-  var storing = Math.floor(Math.random() * character.length);
-
-  // Add character at the random position
-  type = type + [character];
-} 
-
-const confirmPassword= type.join('');
 
 var generateBtn = document.querySelector("#generate");
 
